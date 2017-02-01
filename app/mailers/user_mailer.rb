8ed854528ca71a82ee49a8cf9ada124e7f776291
Tim Parsons <tim.m.parsons@hotmail.com>
2017-02-01 13:55:41 -0800

    5.10

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
 	 		:to => "tim@timmichaelcreates",
 	 		:subject => "A new contact form message from #{name}")
 	 end
+
+	 def welcome(user)
+	 	@appname = "Hit The Lip"
+	 	mail( :to => user.email,
+	 		  :subject => "Welcome to #{@appname}!")
+	 end
 end
