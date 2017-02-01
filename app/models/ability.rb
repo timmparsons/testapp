f8137bcac7a2938826c44d05a243e32daf498491
Tim Parsons <tim.m.parsons@hotmail.com>
2017-02-01 13:38:23 -0800

    5.10

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,13 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
   user ||= User.new # guest user (not logged in)
-   can :manage, User, id:  user.id
+   if user.admin?
+    can :manage, :all
+  else
+    can :read, :all
+  end
+ end
+end
   
     #   if user.admin?
     #     can :manage, :all
