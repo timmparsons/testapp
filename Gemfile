4ff951aa43fdc9770abb0b2c8a3e95940322654c
Tim Parsons <tim.m.parsons@hotmail.com>
2017-02-01 15:57:29 -0800

    6.3

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'devise'
 gem 'cancancan', '~> 1.10'
 
 gem 'will_paginate', '~> 3.1.0'
+
+gem 'rails-controller-testing'
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -36,5 +36,4 @@ end
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-  end
-end
+
