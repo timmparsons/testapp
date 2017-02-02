d9a417e3e0b68b22204bae358e79b795f53053a5
Tim Parsons <tim.m.parsons@hotmail.com>
2017-02-02 14:48:04 -0800

    6.9 JavaScript

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,18 @@
 $(document).on('turbolinks:load', function(){
-	$('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets',
 	readOnly: true,
 	score: function() {
 		return $(this).attr('data-score')
-		}
+		} 
+	});
+	//A jQuery image zoom plugin
+	
+	$('.img-zoom').elevateZoom({
+  	zoomType: "inner",
+  	cursor: "crosshair"
 	});
 });
+
+
+
