d5e5f0b6931825fab9270350853cbdc433358904
Tim Parsons <tim.m.parsons@hotmail.com>
2017-02-03 15:29:28 -0800

    6.10: AJAX

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,16 +1,22 @@
-$(document).on('turbolinks:load', function(){
+var refreshRating = funtion() {
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets',
 	readOnly: true,
 	score: function() {
 		return $(this).attr('data-score')
-		} 
+		}
+	});
+};
+
+
+$(document).on('turbolinks:load', function(){
+	refreshRating();
 	});
 	//A jQuery image zoom plugin
 	
 	$('.img-zoom').elevateZoom({
- 	});
 });
+
 
 
 
