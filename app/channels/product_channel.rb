6ce72d29c8daa7762a3775edc6b096c32fe6b7f0
Tim Parsons <tim.m.parsons@hotmail.com>
2017-02-04 23:57:52 -0800

    6.14

---
+++ app/channels/product_channel.rb
@@ -0,0 +1,17 @@
+class ProductChannel < ApplicationCable::Channel
+  def subscribed
+     #stream_from "product_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+   	
+  end
+
+  def listen(data)
+  	stop_all_streams
+  	steam_for data["product_id"]
+
+  end
+
+end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,15 @@ class CommentsController < ApplicationController
 		@product = Product.find(params[:product_id])
 		@comment = @product.comments.new(comment_params)
 		@comment.user = current_user
+		@user = current_user
 		respond_to do |format|
 		if @comment.save
+			#ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+			
 			format.html { redirect_to @product, notice: "Review was created successfully."}
 			format.json { render :show, status: :created, location: @product }
 			format.js
+
 		else
 			format.html { redirect_to @product, alert: "Review was not saved successfully"}
 			format.json { render json: @comment.errors, status: :unprocessable_entity }
