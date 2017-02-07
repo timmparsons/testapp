59088022f1699bd2c3a9b7c2f6242463d876c9e6
Tim Parsons <tim.m.parsons@hotmail.com>
2017-02-07 15:19:14 -0800

    6.10 revisions

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,11 @@
 var refreshRating = function() {
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets',
-	readOnly: true,
-	score: function() {
+	readOnly: true,	score: function() {
 		return $(this).attr('data-score');
 		}
 	});
-};
+}
 
 
 $(document).on('turbolinks:load ajaxSuccess', function(){
