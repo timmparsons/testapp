703202cd34870d4922de712334a7da6b66c0dabf
Tim Parsons <tim.m.parsons@hotmail.com>
2017-02-20 23:24:20 -0800

    updated homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "user_registrations" }
   resources :users
   resources :products do
   resources :comments
   end
   resources :orders, only: [:index, :show, :create, :destroy]
-  post 'payments/create' 
+  post 'payments/create'
 
   get 'static_pages/about'
 
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get 'static_pages/index'
 
   post 'static_pages/thank_you'
-  
+
 
   root 'static_pages#landing_page'
 
